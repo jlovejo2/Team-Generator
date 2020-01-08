579767b2f17bf53b3eea41d672ae26a133be3178
James Lovejoy <james.lovejoy2@gmail.com>
2020-01-07 18:47:07 -0600

    added start, createManager, addEmployee, printinfo, checkContinuation functionality to createTeam.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const createTeam = require("./lib/createTeam");
+
+//initiliaze a team object
+const team = new createTeam;
+
+//begin running the code that will prompt and create subsequent team members
+team.start();
